84c472737f36fb5a7b08b5e3eb600b66cd9a0128
Shineylo <bryan.artiges@gmail.com>
2023-04-03 16:17:21 +0200

    Routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AccueilComponent } from './Components/accueil/accueil.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    HeaderComponent,
+    FooterComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+       AppRoutingModule,
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
